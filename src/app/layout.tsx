72072fdc046e3a2c61f51141803b529c94b4865f
HIppei <ip.horiba@gmail.com>
2023-10-13 15:13:39 +0900

    Change the title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Auth Client',
+  title: 'Auth Client-2',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
